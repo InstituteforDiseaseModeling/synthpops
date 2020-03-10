3a5a09c8b62ffddbaa65fbf2c93f8d53889e60fd
cliffckerr <questionably@gmail.com>
2020-03-09 22:31:25 -0700

    config is working

--- synthpops/config.py
+++ synthpops/config.py
@@ -1,23 +1,38 @@
+'''
+The point of this file is to set the location of the data folder.
+'''
+
+#%% Housekeeping
+
 import os
 
-__all__ = ['datadir' ,'set_datadir']
+__all__ = ['datadir' ,'set_datadir', 'validate']
 
+# This whole point!
+datadir = None
+
+# Set user-specific configurations
 username = os.path.split(os.path.expanduser('~'))[-1]
 datadirdict = {
-    'dmistry': os.path.join('/home','dmistry','Dropbox (IDM)','seattle_network'),
+    'dmistry': '/home/dmistry/Dropbox (IDM)/seattle_network',
     'cliffk': '/home/cliffk/idm/covid-19/data/seattle_network',
 }
 
+# Try to find the folder on load
 if username in datadirdict.keys():
     datadir = datadirdict[username]
+    print(f'synthpops: your username was found; your data folder is {datadir}.')
 else:
-    print(f'Note: your username, "{username}", is not configured. Please use synthpops.set_datadir() to configure.')
+    print(f'synthpops: your username "{username}" is not configured. Please use synthpops.set_datadir() to configure.')
 
+
+#%% Functions
 
 def set_datadir(folder):
     ''' Set the folder to something user-specific '''
+    global datadir
     datadir = folder
-    print(f'Done; data directory set to {folder}')
+    print(f'Done; data directory set to {folder}.')
     return
 
 
