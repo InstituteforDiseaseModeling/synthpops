59c5b230b3414696b968116eedab439aeebae216
dmistry-idm <dmistry@idmod.org>
2020-04-05 09:53:49 -0700

    test_webapp_synthpops.py cleaned up for release

--- tests/test_webapp_synthpops.py
+++ tests/test_webapp_synthpops.py
@@ -4,44 +4,44 @@ import sciris as sc
 
 default_n = 1000
 
-def test_webapp_synthpops_calls(n=default_n,location='seattle_metro',state_location='Washington',use_bayesian=True,sheet_name='United States of America'):
+def test_webapp_synthpops_calls(n=default_n,location='seattle_metro',state_location='Washington',sheet_name='United States of America'):
     datadir = sp.datadir
 
-    sp.read_age_bracket_distr(datadir,location,state_location=state_location,use_bayesian=use_bayesian)
-    sp.get_census_age_brackets(datadir,use_bayesian=use_bayesian)
+    sp.read_age_bracket_distr(datadir,location=location,state_location=state_location)
+    sp.get_census_age_brackets(datadir,state_location=state_location,country_location=country_location)
     num_agebrackets = 16
 
-    n_contacts_dic = {'H': 4.11, 'S': 11.41, 'W': 8.07, 'R': 7}
+    n_contacts_dic = {'H': 4.11, 'S': 11.41, 'W': 8.07, 'C': 7}
 
-    contact_matrix_dic = sp.get_contact_matrix_dic(datadir,location,num_agebrackets,use_bayesian,sheet_name)
+    contact_matrix_dic = sp.get_contact_matrix_dic(datadir,sheet_name=sheet_name)
     contact_matrix_dic['M'] = sp.combine_matrices(contact_matrix_dic,n_contacts_dic,num_agebrackets)
 
     for k in contact_matrix_dic:
         print(contact_matrix_dic[k].shape)
 
     n = int(n)
-    sp.get_age_n(n=default_n,location=location,state_location=state_location,use_bayesian=use_bayesian)
+    sp.get_age_n(datadir,n=default_n,location=location,state_location=state_location)
 
     return
 
 
-def test_webapp_contacts_calls(n=default_n,location='seattle_metro',state_location='Washington',use_bayesian=True,sheet_name='United States of America'):
+def test_webapp_contacts_calls(n=default_n,location='seattle_metro',state_location='Washington',sheet_name='United States of America'):
 
     n = int(n)
-    popdict = sp.make_popdict(n=n,state_location=state_location,location=location,use_bayesian=True)
+    popdict = sp.make_popdict(n=n,state_location=state_location,location=location)
 
     print(popdict)
 
     return
 
 
-def test_webapp_make_contacts(n=default_n,n_contacts_dic=None,state_location='Washington',location='seattle_metro',use_bayesian=True,sheet_name='Pakistan'):
-    sc.heading(f'Making popdict for {n} peoplse')
+def test_webapp_make_contacts(n=default_n,n_contacts_dic=None,state_location='Washington',location='seattle_metro',country_location='usa',sheet_name='Pakistan'):
+    sc.heading(f'Making popdict for {n} people')
     n = int(n)
-    popdict = sp.make_popdict(n=n,state_location=state_location,location=location,use_bayesian=use_bayesian)
+    popdict = sp.make_popdict(n=n,state_location=state_location,location=location,country_location=country_location)
 
-    options_args = dict.fromkeys(['use_age','use_bayesian'], True)
-    contacts = sp.make_contacts(popdict,n_contacts_dic=n_contacts_dic,state_location=state_location,location=location,sheet_name=sheet_name,options_args=options_args)
+    options_args = dict.fromkeys(['use_age','use_age_mixing'], True)
+    contacts = sp.make_contacts(popdict,n_contacts_dic=n_contacts_dic,state_location=state_location,location=location,country_location=country_location,sheet_name=sheet_name,options_args=options_args)
 
     uids = contacts.keys()
     uids = [uid for uid in uids]
@@ -58,13 +58,13 @@ def test_webapp_make_contacts(n=default_n,n_contacts_dic=None,state_location='Wa
     return popdict
 
 
-def test_webapp_make_contacts_and_show_some_layers(n=default_n,n_contacts_dic=None,state_location='Washington',location='seattle_metro',use_bayesian=True,sheet_name='United States of America'):
+def test_webapp_make_contacts_and_show_some_layers(n=default_n,n_contacts_dic=None,state_location='Washington',location='seattle_metro',country_location='usa',sheet_name='United States of America'):
     sc.heading(f'Making popdict for {n} people')
     n = int(n)
-    popdict = sp.make_popdict(n=n,state_location=state_location,location=location,use_bayesian=use_bayesian)
+    popdict = sp.make_popdict(n=n,state_location=state_location,location=location,country_location=country_location)
 
-    options_args = dict.fromkeys(['use_age','use_social_layers','use_bayesian'], True)
-    contacts = sp.make_contacts(popdict,n_contacts_dic=n_contacts_dic,state_location=state_location,location=location,sheet_name=sheet_name,options_args=options_args)
+    options_args = dict.fromkeys(['use_age','use_social_layers','use_age_mixing'], True)
+    contacts = sp.make_contacts(popdict,n_contacts_dic=n_contacts_dic,state_location=state_location,location=location,country_location=country_location,sheet_name=sheet_name,options_args=options_args)
 
     uids = contacts.keys()
     uids = [uid for uid in uids]
@@ -100,8 +100,7 @@ if __name__ == '__main__':
 
     n = int(3e3)
 
-
-    test_webapp_synthpops_calls(n,location,state_location,use_bayesian,sheet_name)
-    # test_webapp_contacts_calls(n,location,state_location,use_bayesian,sheet_name)
+    test_webapp_synthpops_calls(n,location=location,state_location=state_location,sheet_name=sheet_name)
+    test_webapp_contacts_calls(n,location=location,state_location=state_location,sheet_name=sheet_name)
     test_webapp_make_contacts(n,state_location=state_location,location=location)
     test_webapp_make_contacts_and_show_some_layers(n,state_location=state_location,location=location)
