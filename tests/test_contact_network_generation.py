50761f852b170697f2df8f6563e655ff1f6fe386
dmistry-idm <dmistry@idmod.org>
2020-04-05 09:58:07 -0700

    generates contact network with microstructure

---
+++ tests/test_contact_network_generation.py
@@ -0,0 +1,26 @@
+import synthpops as sp
+import sciris as sc
+import numpy as np
+import math
+import os, sys
+from copy import deepcopy
+
+
+if __name__ == '__main__':
+    sp.validate()
+    datadir = sp.datadir
+
+    state_location = 'Washington'
+    location = 'seattle_metro'
+    country_location = 'usa'
+    sheet_name = 'United States of America'
+    level = 'county'
+
+    n = 12000
+    verbose = False
+    # plot = True
+    plot = False
+
+
+
+    sp.generate_synthetic_population(n,datadir,location=location,state_location=state_location,country_location=country_location,sheet_name=sheet_name,level=level,verbose=verbose,plot=plot)
