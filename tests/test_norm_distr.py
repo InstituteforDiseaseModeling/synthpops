60ac6cd2e7c8a6dbaba1ddcb83bff7dbec2a9a9a
dmistry-idm <dmistry@idmod.org>
2020-05-05 14:22:21 -0700

    testing that synthpops sample_single method works

--- tests/test_norm_distr.py
+++ tests/test_norm_distr.py
@@ -6,8 +6,8 @@ if __name__ == '__main__':
     
     dic = dict()
     dic[0] = 0
-    dic[1] = 1
-    dic[2] = -1
+    dic[1] = 0
+    dic[2] = 0
 
     distr = dic
     # distr = sp.norm_dic(dic)
@@ -18,11 +18,15 @@ if __name__ == '__main__':
     sorted_distr = np.array([distr[k] for k in sorted_keys], dtype=float)
     print(sorted_distr)
 
-    norm_sorted_distr = np.maximum(0,sorted_distr) # Don't allow negatives
-    print(norm_sorted_distr)
+    norm_sorted_distr = np.maximum(0,sorted_distr) # Don't allow negatives- masks negative values to 0
+    print(norm_sorted_distr,'n')
 
-    norm_sorted_distr = norm_sorted_distr/norm_sorted_distr.sum() # Ensure it sums to 1
+    if np.sum(norm_sorted_distr) > 0:
+        norm_sorted_distr = norm_sorted_distr/norm_sorted_distr.sum() # Ensure it sums to 1
     print(norm_sorted_distr)
 
     n = np.random.multinomial(1, norm_sorted_distr, size=1)[0]
     print(n)
+
+    ### synthpops method ###
+    print(sp.sample_single(dic))
