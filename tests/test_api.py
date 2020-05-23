3983ec8a2f10348f850a6564c076bad2ce36dcaa
cliffckerr <questionably@gmail.com>
2020-05-23 13:50:19 -0700

    make api tests shorter

--- tests/test_api.py
+++ tests/test_api.py
@@ -11,7 +11,7 @@ if not sp.config.full_data_available:
 
 def test_api():
 
-    n = 50000
+    n = 2000
     max_contacts = {'S': 20, 'W': 10}
 
     population = sp.make_population(n=n, max_contacts=max_contacts)
@@ -22,13 +22,10 @@ def test_api():
     return population
 
 
-def test_plot_pop():
+def test_plot_pop(do_show=False, pause=0.2):
 
     plotconnections = True
-
-    doclear = False
-    pause = 0.2
-    n = 20000
+    n = 5000
     alpha = 0.5
 
     # indices = pl.arange(1000)
@@ -50,46 +47,47 @@ def test_plot_pop():
     f_inds = [ind for ind, person in enumerate(people) if not person.sex]
     m_inds = [ind for ind, person in enumerate(people) if person.sex]
 
-    # import matplotlib.pyplot as plt
-    # import matplotlib.colors as colors
-    # colors_undersea = plt.cm.terrain(np.linspace(0, 0.17, 256))
-    # colors_land = plt.cm.terrain(np.linspace(0.25, 1, 256))
-    # all_colors = np.vstack((colors_undersea, colors_land))
-    # terrain_map = colors.LinearSegmentedColormap.from_list('terrain_map',
-    #     all_colors)
-    # pl.set_cmap(terrain_map)
+    if do_show:
 
-    fig = pl.figure(figsize=(24, 18))
-    ax = pl.subplot(111)
-    # sc.turbocolormap(apply=True)
-    minval = 0  # ages.min()
-    maxval = 100  # ages.min()
-    colors = sc.vectocolor(ages, minval=minval, maxval=maxval)
-    for i, inds in enumerate([f_inds, m_inds]):
-        pl.scatter(x[inds], y[inds], marker='os'[i], c=colors[inds])
-    pl.clim([minval, maxval])
-    pl.colorbar()
+        use_terrain = False
+        if use_terrain:
+            import matplotlib.pyplot as plt
+            import matplotlib.colors as colors
+            colors_undersea = plt.cm.terrain(np.linspace(0, 0.17, 256))
+            colors_land = plt.cm.terrain(np.linspace(0.25, 1, 256))
+            all_colors = np.vstack((colors_undersea, colors_land))
+            terrain_map = colors.LinearSegmentedColormap.from_list('terrain_map',
+                all_colors)
+            pl.set_cmap(terrain_map)
 
-    if plotconnections:
-        lcols = dict(H=[0, 0, 0], S=[0, 0.5, 1], W=[0, 0.7, 0], C=[1, 1, 0])
-        for index in indices:
-            person = people[index]
-            contacts = person.contacts
-            lines = []
-            for lkey in lcols.keys():
-                for contactkey in contacts[lkey]:
-                    contact = population[contactkey]
-                    tmp = pl.plot([person.loc.x, contact.loc.x], [person.loc.y, contact.loc.y], c=lcols[lkey], alpha=alpha)
-                    lines.append(tmp)
-            # pl.title(f'Index: {index}')
-            # pl.pause(pause)
-            # if doclear:
-                # ax.lines = []
+        fig = pl.figure(figsize=(24, 18))
+        pl.subplot(111)
+        minval = 0  # ages.min()
+        maxval = 100  # ages.min()
+        colors = sc.vectocolor(ages, minval=minval, maxval=maxval)
+        for i, inds in enumerate([f_inds, m_inds]):
+            pl.scatter(x[inds], y[inds], marker='os'[i], c=colors[inds])
+        pl.clim([minval, maxval])
+        pl.colorbar()
 
-    return fig
+        if plotconnections:
+            lcols = dict(H=[0, 0, 0], S=[0, 0.5, 1], W=[0, 0.7, 0], C=[1, 1, 0])
+            for index in indices:
+                person = people[index]
+                contacts = person.contacts
+                lines = []
+                for lkey in lcols.keys():
+                    for contactkey in contacts[lkey]:
+                        contact = population[contactkey]
+                        tmp = pl.plot([person.loc.x, contact.loc.x], [person.loc.y, contact.loc.y], c=lcols[lkey], alpha=alpha)
+                        lines.append(tmp)
+                if pause:
+                    pl.pause(pause)
+
+        return fig
 
 
 if __name__ == '__main__':
     pop1 = test_api()
-    # pop2 = test_plot_pop()
+    fig  = test_plot_pop(do_show=True)
     print('Done.')
