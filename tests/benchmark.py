60f1990dc64c7f6bc38c4daf1a6535b311a47767
MeWu-IDM <40474438+MeWu-IDM@users.noreply.github.com>
2020-07-09 14:17:33 -0700

    add docstrings to all methods

--- tests/benchmark.py
+++ tests/benchmark.py
@@ -1,5 +1,14 @@
-# Benchmark and profile SynthPops
+"""Benchmark and profile SynthPops
 
+This script produces detail profiling results for
+synthpops::make_population method
+it takes parameter "n" as input (default to 10001)
+and produces profiling files test_{n}.txt
+
+Example:
+    $ python benchmark.py
+    $ python benchmark.py 10001 20001 30001
+"""
 import sciris as sc
 import synthpops as sp
 import argparse
@@ -20,16 +29,19 @@ func_options = {
     }
 
 def make_pop(n):
+    """
+        run function to create n population for input n
+    """
     #n = [10000, 10001][1] # Use either a pre-generated population, or one that has to be made from scratch
     max_contacts = {'S': 20, 'W': 10}
     population = sp.make_population(n=n, max_contacts=max_contacts)
     return population
 
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser()
-    parser.add_argument('n', nargs='*', default=[10001], help='population')
-    args = parser.parse_args()
-    for n in args.n:
+def run_benchmark(list_of_n):
+    """
+    loop over list of n and output perf profile for each n to test_n.txt
+    """
+    for n in list_of_n:
         saved_stdout = sys.stdout
         with open(f'test_{n}.txt', 'w') as f:
             sys.stdout = f
@@ -39,3 +51,9 @@ if __name__ == '__main__':
         sys.stdout.close()
         sys.stdout = saved_stdout
         print(f'result n={n} : test_{n}.txt')
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('n', nargs='*', default=[10001], help='population')
+    args = parser.parse_args()
+    run_benchmark(args.n)
