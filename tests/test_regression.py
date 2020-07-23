19e6db9fc7e8f33e780e9ee188b908abd0538b7f
meikang wu <mewu@idmod.org>
2020-07-22 18:23:40 -0700

    #153 -use contact matrix measure to determine regression, the distance should be close to 0 assuming using the same n and seed with fixed parameters. Any newly introduced parameters for make_population are expected to default to conform existing behavior for backward compatibility.
    -modify the example plot method to allow population to be passed in and add prefix for the plot title.

--- tests/test_regression.py
+++ tests/test_regression.py
@@ -1,6 +1,7 @@
 """
 Test regressions with fixed seed
 expected files are in the "expected" folder
+the filename has pattern pop_{n}_seed{seed}.json
 """
 
 import unittest
@@ -9,6 +10,9 @@ import shutil
 import tempfile
 import sciris as sc
 import synthpops as sp
+import numpy as np
+from scipy.spatial import distance
+from examples import plot_age_mixing_matrices
 
 class TestRegression(unittest.TestCase):
 
@@ -21,9 +25,18 @@ class TestRegression(unittest.TestCase):
         shutil.rmtree(cls.resultdir, ignore_errors=True)
 
     def test_regression_make_population(self):
-        filename = os.path.join(self.resultdir,'pop.json')
+        n = 10001
         seed = 1001
-        pop = sp.make_population(n=1001, rand_seed=seed, generate=True)
+        filename = os.path.join(self.resultdir,f'pop_{n}_seed{seed}.json')
+        #test parameter should be specific
+        pop = sp.make_population(n=n,
+                                 max_contacts=None,
+                                 with_industry_code= False,
+                                 with_facilities= False,
+                                 use_two_group_reduction= False,
+                                 average_LTCF_degree= 20,
+                                 rand_seed=seed,
+                                 generate=True)
         # if default sort order is not concerned:
         # pop = dict(sorted(pop.items(), key=lambda x: x[0]))
         sc.savejson(filename, pop, indent=2)
@@ -36,7 +49,31 @@ class TestRegression(unittest.TestCase):
             expected_file = os.path.join(os.path.join(os.path.dirname(__file__), 'expected'), os.path.basename(actual_file))
         if not os.path.isfile(expected_file):
             raise FileNotFoundError(expected_file)
-        expected = sc.loadjson(expected_file)
-        actual = sc.loadjson(actual_file)
-        self.assertEqual(actual, expected)
+        expected = self.cast_uid_toINT(sc.loadjson(expected_file))
+        actual = self.cast_uid_toINT(sc.loadjson(actual_file))
+        self.check_similarity(actual, expected)
 
+        # if test failed, look at the figures
+        #fig = plot_age_mixing_matrices.test_plot_generated_contact_matrix(population=expected, title_prefix="expected_")
+        #fig.show()
+        #fig = plot_age_mixing_matrices.test_plot_generated_contact_matrix(population=actual, title_prefix="actual_")
+        #fig.show()
+
+    def check_similarity(self, actual, expected):
+        """
+        Compare two population dictionaries using contact matrix
+        Assuming the canberra distance should be close to zero
+        """
+        for code in ['H', 'W', 'S']:
+            for option in ['density', 'frequency']:
+                print(f"\ncheck:{code} with {option}")
+                actual_matrix = sp.calculate_contact_matrix(actual, density_or_frequency=option, setting_code=code)
+                expected_matrix = sp.calculate_contact_matrix(expected, density_or_frequency=option, setting_code=code)
+                # calculate Canberra distance
+                # assuming they should round to 0
+                d = distance.canberra(actual_matrix.flatten(), expected_matrix.flatten())
+                self.assertEqual(round(d), 0, f"actual distance for {code}/{option} is {str(round(d))}, "
+                                              f"you need to uncommented line 55 to plot the density matrix and investigate!")
+
+    def cast_uid_toINT(self, dict):
+        return {int(key): val for key, val in dict.items()}
