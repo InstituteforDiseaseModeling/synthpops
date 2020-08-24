42f48cb2bb48d8f11effe9128a7cad87ee817929
meikang wu <mewu@idmod.org>
2020-08-24 15:46:56 -0700

    change test examples to run in parallel to save time make run a manual trigger add step to install graphviz

--- tests/run_example.py
+++ tests/run_example.py
@@ -2,11 +2,13 @@ import unittest
 import os
 import pathlib
 import subprocess
+from multiprocessing import Process, Manager, Pool
 
+finalresult = {}
 class TestExample(unittest.TestCase):
 
-    #some example needs external library
-    excluded =['draw_multilayer_network.py']
+    #some examples are under construction
+    excluded =['read_workplaces_with_industry_naics.py']
 
     @classmethod
     def setUpClass(cls) -> None:
@@ -14,7 +16,8 @@ class TestExample(unittest.TestCase):
         cls.resultdir = os.path.join(os.path.dirname(__file__), 'result')
         os.makedirs(cls.resultdir, exist_ok=True)
 
-    def run_examples(self, commandargs, workingdir):
+    @classmethod
+    def run_examples(cls, commandargs, workingdir, resultdir):
         succeeded = False
         cmd = ['python']
         commandargs = [commandargs] if isinstance(commandargs, str) else commandargs
@@ -24,18 +27,20 @@ class TestExample(unittest.TestCase):
             print("run:", str(cmd))
             outfilename = str(commandargs[0]).replace('.py', '.log')
             errfilename = str(commandargs[0]).replace('.py', '.err.log')
-            with open(os.path.join(self.resultdir, outfilename), 'w') as fout,\
-                    open(os.path.join(self.resultdir, errfilename), 'w') as ferr:
+            with open(os.path.join(resultdir, outfilename), 'w') as fout,\
+                    open(os.path.join(resultdir, errfilename), 'w') as ferr:
                 retcode = subprocess.check_call(cmd, shell=False, cwd=workingdir, stdout=fout, stderr=ferr)
             succeeded = True if retcode == 0 else False
-            succeeded = succeeded and self.check_log(os.path.join(self.resultdir, errfilename))
+            succeeded = succeeded and cls.check_log(os.path.join(resultdir, errfilename))
         except subprocess.CalledProcessError as e:
             print("error:", str(cmd))
             #print(str(e))
-        print("result:", succeeded)
-        return succeeded
+        #print("result:", succeeded)
+        succeeded = "passed" if succeeded else "failed"
+        return {commandargs[0]: succeeded}
 
-    def check_log(self, logfile):
+    @classmethod
+    def check_log(cls, logfile):
         if pathlib.Path(logfile).stat().st_size > 0:
             with open(logfile) as f:
                 output = f.readlines()
@@ -44,16 +49,21 @@ class TestExample(unittest.TestCase):
         return True
 
     def test_examples(self):
-        passed = True
-        result = {}
+        files = []
         for root, dirnames, filenames in os.walk(self.example_path):
             for f in filenames:
                 if str(f).endswith('.py') and f not in self.excluded:
                     print("-------------")
                     print(f)
-                    passed = self.run_examples(f, workingdir=root)
-                    result[f] = 'passed' if passed else 'failed'
+                    files.append(f)
 
-        for i in result:
-            print(i, " : ", result[i])
-        self.assertFalse('failed' in result.values())
+        t = [(f, self.example_path, self.resultdir) for f in files]
+        with Pool(processes=4) as pool:
+            result = pool.starmap(self.run_examples, t)
+
+        print("-------------")
+        for r in result:
+            finalresult.update(r)
+            print(r)
+
+        self.assertFalse('failed' in finalresult.values())
