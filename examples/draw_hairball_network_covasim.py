a749115f6ecf4d77381ef760280a62e6b2d50101
cliffckerr <questionably@gmail.com>
2020-08-26 00:24:03 -0700

    update to draw network examples

--- examples/draw_hairball_network_covasim.py
+++ examples/draw_hairball_network_covasim.py
@@ -1,52 +1,26 @@
-import numpy as np
-import networkx as nx
-import synthpops as sp
-import matplotlib as mplt
-import matplotlib.pyplot as plt
-import matplotlib.font_manager as font_manager
-import os
-import cmocean
-import covasim as cova
-
 """
 Please note this examples takes a few minutes to run
 """
 
+import networkx as nx
+import matplotlib as mplt
+import matplotlib.pyplot as plt
+import covasim as cv
 
-try:
-    username = os.path.split(os.path.expanduser('~'))[-1]
-    fontdirdict = {
-        'dmistry': '/home/dmistry/Dropbox (IDM)/GoogleFonts',
-    }
-    if username not in fontdirdict:
-        # add your path to GoogleFonts
-        fontdirdict[username] = os.path.join(os.path.expanduser('~'), 'Dropbox', 'COVASIM-19', 'GoogleFonts')
-
-    font_path = fontdirdict[username]
-
-    fontpath = fontdirdict[username]
-    font_style = 'Roboto_Condensed'
-    fontstyle_path = os.path.join(fontpath, font_style, font_style.replace('_', '') + '-Light.ttf')
-    prop = font_manager.FontProperties(fname=fontstyle_path)
-    mplt.rcParams['font.family'] = prop.get_name()
-except:
-    mplt.rcParams['font.family'] = 'Roboto'
+mplt.rcParams['font.family'] = 'Roboto'
 
 
 def create_sim(TRACE_PROB=None,  TEST_PROB=None, TRACE_TIME=None, TEST_DELAY=None):
 
     # initialize simulation
-    sim = cova.Sim()
+    sim = cv.Sim()
 
     # PARAMETERS
-    pars = {'pop_size': 5e3}  # start with a small pool << actual population e4 # DEBUG
+    pars = {'pop_size': 1e3}  # start with a small pool << actual population e4 # DEBUG
 
     # diagnosed individuals maintain same beta
     pars.update({
         'pop_type': 'synthpops',
-        # 'pop_type': 'hybrid', # synthpops, hybrid
-        'pop_infected': 0,  # Infect none for starters
-        'n_days': 100,  # 40d is long enough for everything to play out
         'beta_layer': {'h': 2.0, 's': 0, 'w': 0, 'c': 0},  # Turn off all but home
     })
 
@@ -61,8 +35,9 @@ if __name__ == '__main__':
     # directory for storing results
     do_run = True
     verbose = False
+    do_save = False
 
-    fn = f'sim_sar.sim'
+    #fn = f'sim_sar.sim'
     sim = create_sim()
     sim.initialize()
 
@@ -93,8 +68,6 @@ if __name__ == '__main__':
         nx.draw(G, ax=ax, node_size=1, width=0.05)
         ax.set_title(titles[layer], fontsize=24)
 
-    fig_path = os.path.join("..", "data", 'demographics', 'contact_matrices_152_countries', 'usa', 'Washington', "figures")
-    if not os.path.exists(fig_path):
-        os.makedirs(fig_path)
-    fig.savefig(f"{fig_path}_seattle_metro_{sim.pars['pop_size']}_contact_networks_covasim.png")
+    if do_save:
+        fig.savefig(f"seattle_metro_{sim.pars['pop_size']}_contact_networks_covasim.png")
     plt.show()
