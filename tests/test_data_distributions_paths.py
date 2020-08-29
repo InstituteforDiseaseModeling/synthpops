b3006f98712a89ca7d66b1fe3de8e0009cf74998
dennis harding <dennish737@gmail.com>
2020-08-29 12:42:10 -0700

    Added unit test cases

--- tests/test_data_distributions_paths.py
+++ tests/test_data_distributions_paths.py
@@ -17,8 +17,7 @@ def test_get_gender_fraction_by_age_path_all_variables():
     dat_file, file_path = sp.get_gender_fraction_by_age_path(location=location, state_location=state_location,
                                                   country_location=country_location)
     print(file_path)
-    assert file_path is not None
-
+    assert file_path is not  None
 
 # def test_get_gender_fraction_by_age_path_country_variable_only():
 #     """
@@ -37,8 +36,106 @@ def test_get_gender_fraction_by_age_path_country_state_variables():
                                                   country_location=country_location)
     assert dat_file is not None
 
+def test_get_gender_fraction_by_age_path_all_variables_Senegal():
+    """
+    Test getting the file path with all input variables
+    """
+    default_country = sp.config.default_country
+    sp.config.set_location_defaults('Senegal')
+    datadir = sp.datadir
+    location = 'Dakar'
+    state_location = 'Dakar'
+    country_location = 'Senegal'
+    dat_file, file_path = sp.get_gender_fraction_by_age_path(location=location, state_location=state_location,
+                                                  country_location=country_location)
+    print(file_path)
+    sp.config.set_location_defaults(default_country)
+    assert file_path is not None
+
+def test_get_census_age_brackets_path_Senegal():
+    """
+    Test getting the file path with all input variables
+    """
+    default_country = sp.config.default_country
+    sp.config.set_location_defaults('Senegal')
+    datadir = sp.datadir
+    location = 'Dakar'
+    state_location = 'Dakar'
+    country_location = 'Senegal'
+    data_file, file_path = sp.get_census_age_brackets_path(datadir, state_location=state_location,
+                                                  country_location=country_location)
+    print(data_file)
+    print(file_path)
+    sp.config.set_location_defaults(default_country)
+    assert file_path is not None
+
+def test_get_school_size_brackets_path_Senegal():
+    """
+    Test getting the file path with all input variables
+    """
+    default_country = sp.config.default_country
+    sp.config.set_location_defaults('Senegal')
+    datadir = sp.datadir
+    location = 'Dakar'
+    state_location = 'Dakar'
+    country_location = 'Senegal'
+    data_file = sp.get_school_size_brackets_path(datadir, location=location, state_location=state_location,  country_location=country_location)
+    print(data_file)
+    sp.config.set_location_defaults(default_country)
+    assert data_file is not None
+
+def test_get_school_sizes_path_Senegal():
+    """
+    Test getting the file path with all input variables
+    """
+    default_country = sp.config.default_country
+    sp.config.set_location_defaults('Senegal')
+    datadir = sp.datadir
+    location = 'Dakar'
+    state_location = 'Dakar'
+    country_location = 'Senegal'
+    data_file = sp.get_school_sizes_path(datadir, location=location, state_location=state_location,  country_location=country_location)
+    print(data_file)
+    sp.config.set_location_defaults(default_country)
+    assert data_file is not  None
+
+def test_get_school_size_distr_by_brackets_path_Senegal():
+    """
+    Test getting the file path with all input variables
+    """
+    default_country = sp.config.default_country
+    sp.config.set_location_defaults('Senegal')
+    datadir = sp.datadir
+    location = 'Dakar'
+    state_location = 'Dakar'
+    country_location = 'Senegal'
+    data_file = sp.get_school_size_distr_by_brackets_path(datadir, location=location, state_location=state_location,  country_location=country_location)
+    print(data_file)
+    sp.config.set_location_defaults(default_country)
+    assert data_file is not None
+
+def test_get_school_size_distr_by_brackets_path_Senegal():
+    """
+    Test getting the file path with all input variables
+    """
+    default_country = sp.config.default_country
+    sp.config.set_location_defaults('Senegal')
+    datadir = sp.datadir
+    location = 'Dakar'
+    state_location = 'Dakar'
+    country_location = 'Senegal'
+    data_file = sp.get_school_size_distr_by_brackets_path(datadir, location=location, state_location=state_location,  country_location=country_location)
+    print(data_file)
+    sp.config.set_location_defaults(default_country)
+    assert data_file is not None
 
 if __name__ == '__main__':
     test_get_gender_fraction_by_age_path_all_variables()
     # test_get_gender_fraction_by_age_path_country_variable_only()
     test_get_gender_fraction_by_age_path_country_state_variables()
+    test_get_gender_fraction_by_age_path_all_variables_Senegal()
+    test_get_census_age_brackets_path_Senegal()
+    test_get_school_size_brackets_path_Senegal()
+    test_get_school_sizes_path_Senegal()
+    test_get_school_size_distr_by_brackets_path_Senegal()
+
