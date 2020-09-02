6314e1abc8d2e9436d45fb1b1540097282cc71ff
dennis harding <dennish737@gmail.com>
2020-09-02 12:48:28 -0700

    Refactored test in test_filePath.py to a class, before adding additional test

--- tests/test_FilePath.py
+++ tests/test_FilePath.py
@@ -1,106 +1,134 @@
+import datetime
+import inspect
+import unittest
+import utilities
+import tempfile
 import os
+import shutil
+import sys
 import synthpops as sp
-import sciris as sc
+from synthpops import cfg
+#import sciris as sc
 import pytest
 
-def test_set_location_defaults_none():
-    sp.config.set_location_defaults()
-    assert sp.config.default_country == 'usa'
-    assert sp.config.default_state == 'Washington'
-    assert sp.config.default_location == 'seattle_metro'
-    assert sp.config.default_sheet_name == 'United States of America'
 
-def test_set_location_defaults_Senegal():
-    sp.config.set_location_defaults('Senegal')
-    assert sp.config.default_country == 'Senegal'
-    assert sp.config.default_state == 'Dakar'
-    assert sp.config.default_location == 'Dakar'
-    assert sp.config.default_sheet_name is None
+class TestFilePath(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls) -> None:
+        cls.rootDir = tempfile.TemporaryDirectory().name
+        cls.initia_default_dir = cfg.datadir
+        os.makedirs(os.path.join(cls.rootDir, "data"), exist_ok=True)
+        cls.dataDir = os.path.join(cls.rootDir, "data")
 
-def test_set_location_defaults_usa():
-    sp.config.set_location_defaults('usa')
-    assert sp.config.default_country == 'usa'
-    assert sp.config.default_state == 'Washington'
-    assert sp.config.default_location == 'seattle_metro'
-    assert sp.config.default_sheet_name == 'United States of America'
+    @classmethod
+    def tearDownClass(cls) -> None:
+        #cls.copy_output()
+        cfg.set_datadir(cls.initia_default_dir, ["demographics","contact_matrices_152_countries"])
+        cfg.set_location_defaults(country="default")
+        for d in [cls.rootDir]:
+            shutil.rmtree(d, ignore_errors=True)
 
-def test_FilePath_init_None():
-    with pytest.raises(NotImplementedError):
-        file_paths = sp.config.FilePaths()
+    def test_FilePath_init_None(cls):
+        with pytest.raises(NotImplementedError):
+            file_paths = sp.config.FilePaths()
 
-def test_FilePath_no_country():
-    with pytest.raises(NotImplementedError):
-        file_paths = sp.config.FilePaths('seattle_metro', 'Washington')
+    def test_FilePath_no_country(cls):
+        with pytest.raises(NotImplementedError):
+            file_paths = sp.config.FilePaths('seattle_metro', 'Washington')
 
-def test_FilePath_country_only_usa():
-    file_paths = sp.config.FilePaths(country='usa')
-    assert file_paths is not None
-    assert file_paths.country is not None
-    assert file_paths.country == 'usa'
-    assert file_paths.province is None
-    assert file_paths.location is None
-    assert file_paths.basedirs is not None
-    assert len(file_paths.basedirs) == 1
+    def test_set_location_defaults_none(cls):
+        sp.config.set_location_defaults()
+        assert sp.config.default_country == 'usa'
+        assert sp.config.default_state == 'Washington'
+        assert sp.config.default_location == 'seattle_metro'
+        assert sp.config.default_sheet_name == 'United States of America'
 
-def test_FilePath_country_only_Senegal():
-    file_paths = sp.config.FilePaths(country='Senegal')
-    assert file_paths is not None
-    assert file_paths.country is not None
-    assert file_paths.country == 'Senegal'
-    assert file_paths.province is None
-    assert file_paths.location is None
-    assert file_paths.basedirs is not None
-    assert len(file_paths.basedirs) == 1
+    def test_set_location_defaults_Senegal(cls):
+        sp.config.set_location_defaults('Senegal')
+        assert sp.config.default_country == 'Senegal'
+        assert sp.config.default_state == 'Dakar'
+        assert sp.config.default_location == 'Dakar'
+        assert sp.config.default_sheet_name is None
 
-def test_FilePath_invalid_country():
-    with pytest.raises(FileNotFoundError):
-        file_paths = sp.config.FilePaths(country='Germany')
+    def test_set_location_defaults_usacls(cls):
+        sp.config.set_location_defaults('usa')
+        assert sp.config.default_country == 'usa'
+        assert sp.config.default_state == 'Washington'
+        assert sp.config.default_location == 'seattle_metro'
+        assert sp.config.default_sheet_name == 'United States of America'
 
-# Test valid country state combinations
-def test_FilePath_usa_Washington():
-    file_paths = sp.config.FilePaths(province='Washington', country='usa')
-    assert file_paths is not None
-    assert file_paths.country is not None
-    assert file_paths.country == 'usa'
-    assert file_paths.province is not None
-    assert file_paths.province == 'Washington'
-    assert file_paths.location is None
-    assert file_paths.basedirs is not None
-    assert len(file_paths.basedirs) == 2
 
-def test_FilePath_Senegal_Dakar():
-    file_paths = sp.config.FilePaths(province='Dakar', country='Senegal')
-    assert file_paths is not None
-    assert file_paths.country is not None
-    assert file_paths.country == 'Senegal'
-    assert file_paths.province is not None
-    assert file_paths.province == 'Dakar'
-    assert file_paths.location is None
-    assert file_paths.basedirs is not None
-    assert len(file_paths.basedirs) == 2
+    def test_FilePath_country_only_usa(cls):
+        file_paths = sp.config.FilePaths(country='usa')
+        assert file_paths is not None
+        assert file_paths.country is not None
+        assert file_paths.country == 'usa'
+        assert file_paths.province is None
+        assert file_paths.location is None
+        assert file_paths.basedirs is not None
+        assert len(file_paths.basedirs) == 1
 
-# invalid state
-def test_FilePath_usa_Florida():
-    file_paths = sp.config.FilePaths(province='Florida', country='usa')
-    # note because usa is valid and Florida is no, searches will be at USA level
-    assert file_paths is not None
-    assert file_paths.country is not None
-    assert file_paths.country == 'usa'
-    assert file_paths.province is not None
-    assert file_paths.province == 'Florida'
-    assert file_paths.location is None
-    assert file_paths.basedirs is not None
-    assert len(file_paths.basedirs) == 1
+    def test_FilePath_country_only_Senegal(cls):
+        file_paths = sp.config.FilePaths(country='Senegal')
+        assert file_paths is not None
+        assert file_paths.country is not None
+        assert file_paths.country == 'Senegal'
+        assert file_paths.province is None
+        assert file_paths.location is None
+        assert file_paths.basedirs is not None
+        assert len(file_paths.basedirs) == 1
 
-# test valid locations
-def test_FilePath_usa_Washington_seattle_metro():
-    file_paths = sp.config.FilePaths(location='seattle_metro', province='Washington', country='usa')
-    assert file_paths is not None
-    assert file_paths.country is not None
-    assert file_paths.country == 'usa'
-    assert file_paths.province is not None
-    assert file_paths.province == 'Washington'
-    assert file_paths.location is not None
-    assert file_paths.location == 'seattle_metro'
-    assert file_paths.basedirs is not None
-    assert len(file_paths.basedirs) == 3
+    def test_FilePath_invalid_country(cls):
+        with pytest.raises(FileNotFoundError):
+            file_paths = sp.config.FilePaths(country='Germany')
+
+    # Test valid country state combinations
+    def test_FilePath_usa_Washington(cls):
+        file_paths = sp.config.FilePaths(province='Washington', country='usa')
+        assert file_paths is not None
+        assert file_paths.country is not None
+        assert file_paths.country == 'usa'
+        assert file_paths.province is not None
+        assert file_paths.province == 'Washington'
+        assert file_paths.location is None
+        assert file_paths.basedirs is not None
+        assert len(file_paths.basedirs) == 2
+
+    def test_FilePath_Senegal_Dakar(cls):
+        file_paths = sp.config.FilePaths(province='Dakar', country='Senegal')
+        assert file_paths is not None
+        assert file_paths.country is not None
+        assert file_paths.country == 'Senegal'
+        assert file_paths.province is not None
+        assert file_paths.province == 'Dakar'
+        assert file_paths.location is None
+        assert file_paths.basedirs is not None
+        assert len(file_paths.basedirs) == 2
+
+    # invalid state
+    def test_FilePath_usa_Florida(cls):
+        file_paths = sp.config.FilePaths(province='Florida', country='usa')
+        # note because usa is valid and Florida is no, searches will be at USA level
+        assert file_paths is not None
+        assert file_paths.country is not None
+        assert file_paths.country == 'usa'
+        assert file_paths.province is not None
+        assert file_paths.province == 'Florida'
+        assert file_paths.location is None
+        assert file_paths.basedirs is not None
+        assert len(file_paths.basedirs) == 1
+
+    # test valid locations
+    def test_FilePath_usa_Washington_seattle_metro(cls):
+        file_paths = sp.config.FilePaths(location='seattle_metro', province='Washington', country='usa')
+        assert file_paths is not None
+        assert file_paths.country is not None
+        assert file_paths.country == 'usa'
+        assert file_paths.province is not None
+        assert file_paths.province == 'Washington'
+        assert file_paths.location is not None
+        assert file_paths.location == 'seattle_metro'
+        assert file_paths.basedirs is not None
+        assert len(file_paths.basedirs) == 3
+
+
