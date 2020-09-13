eaa647256dd826057ce65837e67b9e0c706614c9
cliffckerr <questionably@gmail.com>
2020-09-12 19:55:41 -0700

    high performance sampler

--- synthpops/sampling.py
+++ synthpops/sampling.py
@@ -6,6 +6,8 @@ import pandas as pd
 import sciris as sc
 import numba as nb
 import random
+import itertools
+import bisect
 from collections import Counter
 from . import base as spb
 from . import data_distributions as spdata
@@ -34,7 +36,21 @@ def set_seed(seed=None):
     return
 
 
-# @nb.njit((nb.int64[:], nb.float64[:]))
+def fast_choice(weights):
+    """
+    Choose an option -- quickly -- from the provided weights. Weights do not need
+    to be normalized.
+
+    Reimplementation of random.choices(), removing the junk.
+
+    Example:
+        fast_choice([0.1,0.2,0.3,0.2,0.1]) # might return 2
+    """
+    cum_weights = list(itertools.accumulate(weights))
+    return bisect.bisect(cum_weights, random._inst.random()*(cum_weights[-1]), 0, len(cum_weights)-1)
+
+
+# @nb.njit(cache=True)
 def sample_single_dict(distr_keys, distr_vals):
     """
     Sample from a distribution.
