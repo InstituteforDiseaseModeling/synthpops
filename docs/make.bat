2b80c0c7db01ab88ed2e24df7abb366e80313aa0
Lauren George <lgeorge@idmod.org>
2020-09-29 22:27:51 -0700

    fix broken edit on github link

--- docs/make.bat
+++ docs/make.bat
@@ -5,7 +5,7 @@ REM Command file for Sphinx documentation
 if "%SPHINXBUILD%" == "" (
 	set SPHINXBUILD=sphinx-build
 )
-set SPHINXOPTS=-W
+set "SPHINXOPTS=-W -A READTHEDOCS=True"
 set BUILDDIR=_build
 set ALLSPHINXOPTS=-d %BUILDDIR%/doctrees %SPHINXOPTS% .
 set I18NSPHINXOPTS=%SPHINXOPTS% .
