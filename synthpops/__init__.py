fcd3e132e0e278cb24d1d7e8967d3d7c042063d0
cliffckerr <questionably@gmail.com>
2020-11-03 15:15:11 -0800

    remove unused files

--- synthpops/__init__.py
+++ synthpops/__init__.py
@@ -3,11 +3,9 @@ from .config import *
 from .base import *
 from .data_distributions import *
 from .sampling import *
-from .contacts import *
 from .contact_networks import *
 from .plot_tools import *
 from .api import *
 from .long_term_care_facilities import *
 from .school_modules import *
-from .read_write import *
 logger.debug('Finished imports')
