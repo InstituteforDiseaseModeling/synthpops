abc267b892156a5262b86eef819306508b3bd788
cliffckerr <questionably@gmail.com>
2020-11-04 12:22:44 -0800

    refactored data distribution tests

--- tests/unittests/test_data_distributions.py
+++ tests/unittests/test_data_distributions.py
@@ -1,7 +1,6 @@
 import os
 import synthpops as sp
-import sciris as sc
-import pytest
+from synthpops import data_distributions as spdd
 
 datadir = sp.datadir
 location = 'seattle_metro'
@@ -12,48 +11,48 @@ country_location = 'usa'
 # country_location = 'Senegal'
 
 
-def test_get_gender_fraction_by_age_path():
-    dat_file, file_path = sp.get_gender_fraction_by_age_path(location=location, state_location=state_location,
-                                                  country_location=country_location)
-    assert file_path is not None
+# def test_get_gender_fraction_by_age_path():
+#     dat_file, file_path = spdd.get_gender_fraction_by_age_path(location=location, state_location=state_location,
+#                                                   country_location=country_location)
+#     assert file_path is not None
 
 
-def test_read_gender_fraction_by_age():
-    gender_data_file_path = os.path.join(datadir, 'demographics', 'contact_matrices_152_countries',
-                                         country_location, state_location, 'age_distributions',
-                                         'seattle_metro_gender_fraction_by_age_bracket.dat')
-    dict = sp.read_gender_fraction_by_age_bracket(datadir, state_location=state_location,
-                                                  country_location=country_location,
-                                                  file_path=gender_data_file_path, use_default=True)
-    assert dict is not None
+# def test_read_gender_fraction_by_age():
+#     gender_data_file_path = os.path.join(datadir, 'demographics', 'contact_matrices_152_countries',
+#                                          country_location, state_location, 'age_distributions',
+#                                          'seattle_metro_gender_fraction_by_age_bracket.dat')
+#     dict = spdd.read_gender_fraction_by_age_bracket(datadir, state_location=state_location,
+#                                                   country_location=country_location,
+#                                                   file_path=gender_data_file_path, use_default=True)
+#     assert dict is not None
 
 
 def test_get_age_bracket_distr_path():
-    dat_file = sp.get_age_bracket_distr_path(location=location, state_location=state_location,
+    dat_file = spdd.get_age_bracket_distr_path(location=location, state_location=state_location,
                                              country_location=country_location)
     assert dat_file is not None
 
 
 def test_get_household_size_distr_path():
-    dat_file = sp.get_household_size_distr_path(datadir=datadir, location=location, state_location=state_location,
+    dat_file = spdd.get_household_size_distr_path(datadir=datadir, location=location, state_location=state_location,
                                                 country_location=country_location)
     assert dat_file
 
 
 def test_get_head_age_brackets_path():
-    dat_file = sp.get_head_age_brackets_path(datadir=datadir, state_location=state_location,
+    dat_file = spdd.get_head_age_brackets_path(datadir=datadir, state_location=state_location,
                                              country_location=country_location)
     assert dat_file is not None
 
 
 def test_get_household_head_age_by_size_path():
-    dat_file = sp.get_household_head_age_by_size_path(datadir=datadir, state_location=state_location,
+    dat_file = spdd.get_household_head_age_by_size_path(datadir=datadir, state_location=state_location,
                                                       country_location=country_location)
     assert dat_file is not None
 
 
 def test_get_head_age_by_size_path():
-    hha_by_size = sp.get_household_head_age_by_size_path(datadir, state_location=state_location,
+    hha_by_size = spdd.get_household_head_age_by_size_path(datadir, state_location=state_location,
                                                          country_location=country_location)
     assert hha_by_size is not None
 
@@ -63,7 +62,7 @@ def test_get_school_enrollment_rates():
                                                 country_location,
                                                 state_location,
                                                 'household_head_age_and_size_count.dat')
-    dict = sp.get_school_enrollment_rates(datadir, location=location, state_location=state_location,
+    dict = spdd.get_school_enrollment_rates(datadir, location=location, state_location=state_location,
                                           country_location=country_location,
                                           file_path=school_enrollement_file_path, use_default=True)
     assert dict is not None
@@ -72,14 +71,14 @@ def test_get_school_enrollment_rates():
 def test_get_contact_matrix():
     setting_code = 'H'
     sheet_name = 'United States of America'
-    data_matrix = sp.get_contact_matrix(datadir, setting_code, sheet_name=sheet_name)
+    data_matrix = spdd.get_contact_matrix(datadir, setting_code, sheet_name=sheet_name)
     assert len(data_matrix) == 16
 
 
 if __name__ == '__main__':
     # We currently have files for both Senegal and USA for this data
-    test_get_gender_fraction_by_age_path()
-    test_read_gender_fraction_by_age()
+    # test_get_gender_fraction_by_age_path()
+    # test_read_gender_fraction_by_age()
     test_get_school_enrollment_rates()
     test_get_head_age_brackets_path()
     test_get_contact_matrix()
@@ -88,4 +87,4 @@ if __name__ == '__main__':
     test_get_household_size_distr_path()
     test_get_household_head_age_by_size_path()
     test_get_head_age_by_size_path()
-    test_get_gender_fraction_by_age_path()
+    # test_get_gender_fraction_by_age_path()
