4b6f10eba7403f77a1a3caa375850fc649009a06
cliffckerr <questionably@gmail.com>
2021-01-13 00:24:26 -0800

    dakar test passes

--- tests/test_Dakar.py
+++ tests/test_Dakar.py
@@ -18,24 +18,7 @@ pars = dict(
     with_facilities                 = 0,
     with_non_teaching_staff         = 1,
     use_two_group_reduction         = 1,
-    with_school_types               = 1,
-
-    average_LTCF_degree             = 20,
-    ltcf_staff_age_min              = 20,
-    ltcf_staff_age_max              = 60,
-
-    school_mixing_type              = 'age_and_class_clustered',
-    average_class_size              = 20,
-    inter_grade_mixing              = 0.1,
-    teacher_age_min                 = 25,
-    teacher_age_max                 = 75,
-    staff_age_min                   = 20,
-    staff_age_max                   = 75,
-
-    average_student_teacher_ratio   = 20,
-    average_teacher_teacher_degree  = 3,
-    average_student_all_staff_ratio = 15,
-    average_additional_staff_degree = 20,
+    with_school_types               = 0,
     )
 
 
