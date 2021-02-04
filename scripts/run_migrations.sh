1f7dc616a815aa987d2594f64d0d23948ca1696b
Ace Thompson <athompson@idmod.org>
2021-02-03 16:43:09 -0800

    Added Senegal locations to migration collector and data files.

--- scripts/run_migrations.sh
+++ scripts/run_migrations.sh
@@ -6,3 +6,10 @@ python scripts/migrate_legacy_data.py --output_folder=data/ --country_location=u
 python scripts/migrate_legacy_data.py --output_folder=data/ --country_location=usa --state_location=Washington
 python scripts/migrate_legacy_data.py --output_folder=data/ --country_location=usa --state_location=Washington --location=seattle_metro
 
+python scripts/migrate_legacy_data.py --output_folder=data/ --country_location=Senegal
+python scripts/migrate_legacy_data.py --output_folder=data/ --country_location=Senegal --state_location=Dakar
+python scripts/migrate_legacy_data.py --output_folder=data/ --country_location=Senegal --state_location=Dakar --location=Dakar
+
+
+
+
