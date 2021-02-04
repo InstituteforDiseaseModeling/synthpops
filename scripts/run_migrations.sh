9825c7b518c3eb9791a7ae8d609c2b0d08288b9d
Ace Thompson <athompson@idmod.org>
2021-02-03 16:57:08 -0800

    Added Oregon data to migration and reordered alphabetically on country, state.

--- scripts/run_migrations.sh
+++ scripts/run_migrations.sh
@@ -2,13 +2,21 @@
 
 # Run this from the base synthpops folder.
 
-python scripts/migrate_legacy_data.py --output_folder=data/ --country_location=usa
-python scripts/migrate_legacy_data.py --output_folder=data/ --country_location=usa --state_location=Washington
-python scripts/migrate_legacy_data.py --output_folder=data/ --country_location=usa --state_location=Washington --location=seattle_metro
-
 python scripts/migrate_legacy_data.py --output_folder=data/ --country_location=Senegal
 python scripts/migrate_legacy_data.py --output_folder=data/ --country_location=Senegal --state_location=Dakar
 python scripts/migrate_legacy_data.py --output_folder=data/ --country_location=Senegal --state_location=Dakar --location=Dakar
+
+python scripts/migrate_legacy_data.py --output_folder=data/ --country_location=usa --state_location=Oregon
+python scripts/migrate_legacy_data.py --output_folder=data/ --country_location=usa --state_location=Oregon --location=portland_metro
+
+python scripts/migrate_legacy_data.py --output_folder=data/ --country_location=usa
+python scripts/migrate_legacy_data.py --output_folder=data/ --country_location=usa --state_location=Washington
+python scripts/migrate_legacy_data.py --output_folder=data/ --country_location=usa --state_location=Washington --location=seattle_metro
+python scripts/migrate_legacy_data.py --output_folder=data/ --country_location=usa --state_location=Washington --location=Spokane_County
+
+
+
+
 
 
 
