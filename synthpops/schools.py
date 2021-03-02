a169de31f321515fd35c582e7ab2a1d80a1d357c
Dina Mistry <dina.c.mistry@gmail.com>
2021-03-02 10:19:25 -0800

    docstring edits

--- synthpops/schools.py
+++ synthpops/schools.py
@@ -749,10 +749,10 @@ def add_school_edges(popdict, syn_school_uids, syn_school_ages, teachers, non_te
         verbose (bool)                          : print statements throughout
 
     Return:
-        Updated popdict.
+        Updated popdict with edges generated in schools.
 
     Notes:
-        average_teacher_teacher_degree will not be used in school_mixing_type == 'random' scenario
+        average_teacher_teacher_degree will not be used in school_mixing_type == 'random' scenario.
     """
     # completely random contacts across the school, no guarantee of contact with a teacher, much like universities
     available_school_mixing_types = ['random', 'age_clustered', 'age_and_class_clustered']
