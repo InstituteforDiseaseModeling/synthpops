58be64d6f16b18cfac63662f0e08e5847d082616
Dina Mistry <dina.c.mistry@gmail.com>
2021-03-06 22:32:54 -0800

    starting to add tests for config module

---
+++ tests/test_config.py
@@ -0,0 +1,40 @@
+"""
+Test config methods.
+"""
+import synthpops as sp
+
+
+def test_version():
+    sp.logger.info("Testing that version info is returned.")
+    sp.version_info()
+
+
+def test_nbrackets():
+    sp.logger.info("Testing that nbrackets can be set outside of the recommended range and warning message returned.")
+    nbrackets = max(min(sp.config.valid_nbracket_ranges), 2)  # make sure new nbrackets is at least 2
+    sp.set_nbrackets(n=nbrackets - 1)  # testing a valid outside the range currently supported.
+
+
+def test_validate_datadir():
+    sp.logger.info("Testing that synthpops.datadir can be found.")
+    sp.validate_datadir()
+    sp.validate_datadir(verbose=False)
+
+
+def test_set_datadir():
+    sp.logger.info("Testing set_datadir still works in essence.")
+    datadir = sp.set_datadir('not_spdatadir')
+    assert datadir != sp.datadir, "Check failed. datadir set still equal to default sp.datadir"
+    print("New datadir set different from synthpops default.")
+
+    datadir = sp.set_datadir(sp.datadir)
+    assert datadir == sp.datadir, "Check failed. datadir did not reset to default sp.datadir"
+    print("datadir reset to synthpops default.")
+
+
+if __name__ == '__main__':
+
+    test_version()
+    test_nbrackets()
+    test_validate_datadir()
+    test_set_datadir()
