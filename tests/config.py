7d8f6a776e5c72a901a3fe133c40014d98b81efe
meikang wu <mewu@idmod.org>
2021-03-08 11:56:53 -0800

    remove old files

--- tests/config.py
+++
@@ -1,6 +0,0 @@
-import os
-
-do_save = eval(os.getenv("do_save", "False"))
-do_show = eval(os.getenv("do_show", "False"))
-artifact_dir = os.getenv("artifact_dir", "artifact")
-os.makedirs(artifact_dir, exist_ok=True)
