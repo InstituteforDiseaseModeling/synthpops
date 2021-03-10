1206eca6bef7a59e6977f70fa131aacdf180e224
Dina Mistry <dina.c.mistry@gmail.com>
2021-03-09 21:56:52 -0800

    higher version for test and adding logger

--- tests/test_config.py
+++ tests/test_config.py
@@ -12,8 +12,9 @@ def test_version():
 
 
 def test_metadata():
+    sp.logger.info("Testing that the version is greater than 1.5.0")
     pop = sp.Pop(n=100)
-    assert sc.compareversions(pop.version, '1.0.0') == 1 # to check that the version of synthpops is higher than 1.0.0
+    assert sc.compareversions(pop.version, '1.5.0') == 1 # to check that the version of synthpops is higher than 1.0.0
 
 
 if __name__ == '__main__':
