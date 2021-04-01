0f77f422a6591beab0b12971aa44e2286a50ef6a
Ace Thompson <athompson@idmod.org>
2021-04-01 11:52:32 -0700

    Marking several tests as skipped.

--- tests/unittests/test_data_distributions.py
+++ tests/unittests/test_data_distributions.py
@@ -28,6 +28,7 @@ country_location = 'usa'
 #     assert dict is not None
 
 
+@pytest.mark.skip(reason="Path methods were removed; we're keeping this around for reference.")
 @pytest.mark.parametrize("nbrackets", [None, "16", "18", "20"])
 def test_get_age_bracket_distr_path(nbrackets):
     dat_file = spdd.get_age_bracket_distr_path(datadir=datadir, location=location, state_location=state_location,
@@ -37,24 +38,28 @@ def test_get_age_bracket_distr_path(nbrackets):
     assert os.path.exists(dat_file)
 
 
+@pytest.mark.skip(reason="Path methods were removed; we're keeping this around for reference.")
 def test_get_household_size_distr_path():
     dat_file = spdd.get_household_size_distr_path(datadir=datadir, location=location, state_location=state_location,
                                                 country_location=country_location)
     assert dat_file
 
 
+@pytest.mark.skip(reason="Path methods were removed; we're keeping this around for reference.")
 def test_get_head_age_brackets_path():
     dat_file = spdd.get_head_age_brackets_path(datadir=datadir, state_location=state_location,
                                              country_location=country_location)
     assert dat_file is not None
 
 
+@pytest.mark.skip(reason="Path methods were removed; we're keeping this around for reference.")
 def test_get_household_head_age_by_size_path():
     dat_file = spdd.get_household_head_age_by_size_path(datadir=datadir, state_location=state_location,
                                                       country_location=country_location)
     assert dat_file is not None
 
 
+@pytest.mark.skip(reason="Path methods were removed; we're keeping this around for reference.")
 def test_get_head_age_by_size_path():
     hha_by_size = spdd.get_household_head_age_by_size_path(datadir, state_location=state_location,
                                                          country_location=country_location)
