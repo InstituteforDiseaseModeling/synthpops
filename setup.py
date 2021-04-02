b1f6c0026fc895d1f385e2b81a931219ff21081a
Dina Mistry <dina.c.mistry@gmail.com>
2021-04-02 00:35:49 -0700

    re-pin line_profiler

--- setup.py
+++ setup.py
@@ -45,7 +45,7 @@ setup(
     include_package_data=True,
     install_requires=[
         "decorator==4.4.2",
-        # "line_profiler==3.1", # Temporary -- build issue with line_profiler 3.2
+        "line_profiler==3.1", # Temporary -- build issue with line_profiler 3.2
         "sciris>=1.0.0",
         "covasim>=2.0.0",
         "matplotlib>=3.3.0",
