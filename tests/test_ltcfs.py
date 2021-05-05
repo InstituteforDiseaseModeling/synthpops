1da5a9363c7e306d21225098d4d94119d1c72054
Dina Mistry <dina.c.mistry@gmail.com>
2021-05-05 10:13:37 -0700

    reduce time again

--- tests/test_ltcfs.py
+++ tests/test_ltcfs.py
@@ -13,7 +13,7 @@ import settings
 
 
 pars = sc.objdict(
-            n                       = settings.pop_sizes.medium,
+            n                       = settings.pop_sizes.small_medium,
             rand_seed               = 123,
 
             with_facilities         = 1,
